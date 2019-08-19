caf57ba6552471ed5c45884c3c93dc7227cea9d2
eadehemingway <eade.hemingway@gmail.com>
2019-08-19 09:36:51 +0300

    import app from app not solutuion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { App } from './components/App';
-import { App } from './components/Solution';
+import { App } from './components/App';
+// import { App } from './components/Solution';
 import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
