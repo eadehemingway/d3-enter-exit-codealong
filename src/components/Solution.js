b478860de27ebfceb733aa676df0569825bb4402
eadehemingway <eade.hemingway@gmail.com>
2019-11-23 15:00:25 +0000

    change the check for isinterger to check if d falsy

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -1,109 +1,124 @@
-import React from 'react';
-import * as d3 from 'd3';
+import React from 'react'
+import * as d3 from 'd3'
 
-export class App extends React.Component{
+export class App extends React.Component {
   static defaultProps = {
     svgWidth: 700,
-    svgHeight: 500,
-
+    svgHeight: 500
   }
-    state = {
-      inputValues:[]
-    }
+  state = {
+    inputValues: []
+  }
 
-componentDidMount(){  
-  const {svgHeight, svgWidth} = this.props
-  d3.select('#chart')
-  .append('svg')
-  .attr('width', svgWidth)
-  .attr('height', svgHeight);
+  componentDidMount() {
+    const { svgHeight, svgWidth } = this.props
+    d3.select('#chart')
+      .append('svg')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight)
+  }
 
-}
+  draw = () => {
+    const { inputValues } = this.state
 
-draw=()=>{
-const {inputValues} = this.state
-const {svgHeight, svgWidth} = this.props
+    const { svgHeight, svgWidth } = this.props
 
-const svg = d3.select('svg')
-const circleSelection  = svg.selectAll('circle').data(inputValues)
+    const svg = d3.select('svg')
+    const circleSelection = svg.selectAll('circle').data(inputValues)
 
-console.log(circleSelection)
-console.log(circleSelection.enter())
+    const enteringCircles = circleSelection
+      .enter()
+      .append('circle')
+      .attr('class', 'circles')
+      .attr('cx', svgWidth / 2)
+      .attr('cy', svgHeight / 2)
+      .attr('r', 0)
+      .attr('stroke', 'salmon')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none')
 
-const enteringCircles = circleSelection
-  .enter()
-  .append('circle')
-  .attr('class', 'circles')
-  .attr('cx', svgWidth/2)
-  .attr('cy', svgHeight/2)
-  .attr('r', 0)
-  .attr('stroke', 'salmon')
-  .attr('stroke-width', 2)
-  .attr('fill', 'none')
+    const updateSelection = circleSelection.merge(enteringCircles)
 
+    updateSelection
+      .transition()
+      .duration(1500)
+      .attr('r', d => (d ? d : 0))
 
-const updateSelection = circleSelection.merge(enteringCircles)
+    circleSelection
+      .exit()
+      .transition()
+      .duration(1500)
+      .attr('r', 0)
+      .remove()
+  }
 
-updateSelection
-  .transition()
-  .duration(1500)
-  .attr('r', d=> (Number.isInteger(d) ? d: 0))
+  addInput = () => {
+    const newinputValues = [...this.state.inputValues, '']
+    this.setState({ inputValues: newinputValues })
+  }
+  removeInput = () => {
+    const newinputValues = [...this.state.inputValues]
+    newinputValues.pop()
+    this.setState({ inputValues: newinputValues })
+  }
 
+  fillRandomly = () => {
+    const newVals = this.state.inputValues.map(v =>
+      Math.floor(Math.random() * (200 - 1) + 1)
+    )
+    this.setState({ inputValues: newVals })
+  }
 
-  circleSelection
-  .exit() 
-  .transition() 
-  .duration(1500)
-  .attr('r', 0)
-  .remove(); 
+  changeInput = (val, i) => {
+    const inputs = [...this.state.inputValues]
+    inputs.splice(i, 1, val)
+    this.setState({ inputValues: inputs })
+  }
 
-}
-
-addInput = () => {
-  const newinputValues = [...this.state.inputValues, '']
-  this.setState({inputValues: newinputValues})
-}
-removeInput = () => {
-  const newinputValues = [...this.state.inputValues]
-  newinputValues.pop()
-  this.setState({inputValues: newinputValues})
-}
-
-fillRandomly = ()=> {
-  const newVals = this.state.inputValues.map(v =>  Math.floor(Math.random() * (200 - 1) + 1));
-  this.setState({inputValues: newVals})
-}
-
-changeInput = (val, i) => {
-  const inputs = [...this.state.inputValues]
-  inputs.splice(i, 1, val)
-  this.setState({inputValues: inputs})
-}
-
-
-  render(){
-    const {inputValues} = this.state
+  render() {
+    const { inputValues } = this.state
     return (
       <section>
-      <div id="chart-container">
-        <div id="control-box">
-          <div id="input-container">
-            {inputValues.map(
-              (input, i)=>  (<input type="text" key={i} className= 'input-box node-inputs' value={input} onChange={(e)=> this.changeInput(e.target.value, i)}/>))}
+        <div id="chart-container">
+          <div id="control-box">
+            <div id="input-container">
+              {inputValues.map((input, i) => (
+                <input
+                  type="text"
+                  key={i}
+                  className="input-box node-inputs"
+                  value={input}
+                  onChange={e => this.changeInput(e.target.value, i)}
+                />
+              ))}
+            </div>
+            <button
+              id="fill"
+              className="fill-input-btn"
+              onClick={this.fillRandomly}
+            >
+              FILL RANDOMLY
+            </button>
           </div>
-          <button id="fill" className="fill-input-btn" onClick={this.fillRandomly}>FILL RANDOMLY</button>
+          <div id="chart"></div>
         </div>
-        <div id="chart"></div>
-      </div>
-  
-      <div className="button-container">
-        <button id="add" className="add-input-btn" onClick={this.addInput}>+</button>
-        <button id="exit" className="delete-input-btn red-border" onClick={this.removeInput}>-</button>
-        <button id="update" className="update-input-btn" onClick={this.draw}>UPDATE</button>
-      </div>
-    </section>
+
+        <div className="button-container">
+          <button id="add" className="add-input-btn" onClick={this.addInput}>
+            +
+          </button>
+          <button
+            id="exit"
+            className="delete-input-btn red-border"
+            onClick={this.removeInput}
+          >
+            -
+          </button>
+          <button id="update" className="update-input-btn" onClick={this.draw}>
+            UPDATE
+          </button>
+        </div>
+      </section>
     )
   }
 }
- 
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components/App';
-// import { App } from './components/Solution';
-import './style.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { App } from './components/App';
+import { App } from './components/Solution'
+import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
